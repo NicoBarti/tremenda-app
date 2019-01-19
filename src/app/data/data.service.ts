0f63012e7fc3e92370879dda4d182bb007d3389e
NicoBarti <nicolas.barticevic@gmail.com>
2019-01-19 09:36:20 -0300

    iniciando data service

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -11,3 +11,19 @@ baseUrl = ''
 
   constructor(private http: HttpClient) { }
 }
+
+
+
+Here's an example in jQuery:
+
+jQuery.ajax({
+  type: 'POST',
+  url: '/people',
+  dataType: 'json',
+  contentType: 'application/json',
+  data: JSON.stringify({ person: { firstName: "Yehuda", lastName: "Katz" } }),
+  success: function(json) { }
+});
+ActionDispatch::Request will see the Content-Type and your parameters will be:
+
+{ :person => { :firstName => "Yehuda", :lastName => "Katz" } }
