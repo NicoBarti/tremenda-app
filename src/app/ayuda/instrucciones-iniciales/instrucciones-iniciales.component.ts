7373fe5b112d243198da09e368c793d4f7b56023
NicoBarti <nicolas.barticevic@gmail.com>
2019-08-13 15:24:11 -0400

    avances en animación de ayuda

--- src/app/ayuda/instrucciones-iniciales/instrucciones-iniciales.component.ts
+++ src/app/ayuda/instrucciones-iniciales/instrucciones-iniciales.component.ts
@@ -15,24 +15,73 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       state('activo', style({ backgroundColor: 'white', padding: '15px', border: '2px', borderColor: '#ff4081', borderStyle: 'solid'})),
       state('inactivo', style({ backgroundColor: 'transparent'})),
      transition('* => activo', [
-       animate('250ms 200ms')
+       animate('250ms 200ms ease-in')
     ]),
     transition('* => inactivo', [
       style({opacity: 1}),
       animate('250ms ease-out')
     ])
+  ]),
+  trigger('explicacionPregunta', [
+    transition(':enter', [
+      style({opacity : 0}),
+      animate('250ms 500ms ease-in', style({opacity: 1}))
     ])
-  ]
+  ]),
+  trigger('respuestasAnimacion', [
+    state('activo', style({ backgroundColor: 'white', padding: '15px', border: '2px', borderColor: '#ff4081', borderStyle: 'solid'})),
+    state('inactivo', style({ backgroundColor: 'transparent'})),
+    transition('* => activo', [
+      animate('250ms 200ms ease-in')
+  ]),
+  transition('* => inactivo', [
+    style({opacity: 1}),
+    animate('250ms ease-out')
+  ])
+]),
+trigger('explicacionRespuestas', [
+  transition(':enter', [
+    style({opacity : 0}),
+    animate('250ms 500ms ease-in', style({opacity: 1}))
+  ])
+]),
+trigger('botonesAnimacion', [
+  state('activo', style({ backgroundColor: 'white', padding: '15px', border: '2px', borderColor: '#ff4081', borderStyle: 'solid', zIndex: '1'})),
+  state('inactivo', style({ backgroundColor: 'transparent'})),
+ transition('* => activo', [
+   animate('250ms 200ms ease-in')
+]),
+transition('* => inactivo', [
+  style({opacity: 1}),
+  animate('250ms ease-out')
+])
+]),
+trigger('explicacionBotones', [
+  transition(':enter', [
+    style({opacity : 0}),
+    animate('250ms 500ms ease-in', style({opacity: 1}))
+  ])
+]),
+]
 })
+
 export class InstruccionesInicialesComponent implements OnInit {
 
   constructor(
     private cuestionarioService: CuestionarioService
   ) { }
 
+  //Variables de contenido para el html, imitando compnente hoja-Respuestas
   respuestaForm: FormGroup;
   respuestas: Preguntas
+
+  //Variables que encienden las distintas partes de la animacion
   animarPregunta: Boolean
+  animarRespuestas: Boolean
+  animarBotones: Boolean
+
+  //Secuenciador
+  i = 0
 
 
   ngOnInit() {
@@ -45,5 +94,17 @@ export class InstruccionesInicialesComponent implements OnInit {
 
   get item() { return this.respuestaForm.get('item')};
 
+siguiente(){
+  this.i++
+  if(this.i == 1){
+    this.animarPregunta = false
+    this.animarRespuestas = true
+  }
+  if(this.i == 2){
+    this.animarRespuestas = false
+    this.animarBotones = true
+  }
+
+}
 
 }
