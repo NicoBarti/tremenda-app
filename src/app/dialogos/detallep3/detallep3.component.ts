8130390daf8c9587d3edd9c919dbf7b5f9bbf0be
NicoBarti <nicolas.barticevic@gmail.com>
2019-09-03 11:33:45 -0400

    cambio orden tragos

--- src/app/dialogos/detallep3/detallep3.component.ts
+++ src/app/dialogos/detallep3/detallep3.component.ts
@@ -25,13 +25,16 @@ datos = []
 
 this.datos = [
   {imagen: LISTA_TRAGOS[0].imagen, nombre: 'Copa de vino 100 mL', eq: 1 },
-  {imagen: LISTA_TRAGOS[3].imagen, nombre: 'Lata de cerveza 375cc', eq: 1 },
-  {imagen: LISTA_TRAGOS[5].imagen, nombre: 'Botella de cerveza 1 litro', eq: 3 },
-  {imagen: LISTA_TRAGOS[8].imagen, nombre: 'Combinado fuerte', eq: 2 },
   {imagen: LISTA_TRAGOS[1].imagen, nombre: 'Vaso de vino 200 mL', eq: 2},
-  {imagen: LISTA_TRAGOS[6].imagen, nombre: 'Corto de licor 30 mL', eq: 1},
   {imagen: LISTA_TRAGOS[2].imagen, nombre: 'Botella de vino 750 mL', eq: 6},
-  {imagen: LISTA_TRAGOS[7].imagen, nombre: 'Combinado simple', eq: 1},
+
+  {imagen: LISTA_TRAGOS[3].imagen, nombre: 'Lata de cerveza 375cc', eq: 1 },
+  {imagen: LISTA_TRAGOS[4].imagen, nombre: 'Shop 500cc', eq: 1.5 },
+  {imagen: LISTA_TRAGOS[5].imagen, nombre: 'Botella de cerveza 1 litro', eq: 3 },
+
+  {imagen: LISTA_TRAGOS[8].imagen, nombre: 'Combinado fuerte', eq: 2 },
+  {imagen: LISTA_TRAGOS[6].imagen, nombre: 'Corto de licor 30 mL', eq: 1},
+  {imagen: LISTA_TRAGOS[7].imagen, nombre: 'Combinado simple', eq: 1}
 
 ]
   }
